15dbd8bc3489f94d57b741aab3fd77fd00ff892d
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2024-05-28 20:56:04 +0800

    Add files via upload

---
+++ sicbo def.py
@@ -0,0 +1,39 @@
+from random import randint
+from time import sleep 
+
+print ('selamat datang di permainan roll dadu tanpa batas ')
+print ('\t','[••• silahkan bermain •••]','\n')
+         
+main = int (input('Masuk kan nilai berapakali ingin bermain = '))
+t = int(input ('jeda waktu = '))
+
+def sc (): # rool dic
+    a = randint (1,6 )
+    b = randint (1,6 )
+    c = randint (1,6 )
+    hasil =int (a + b +c)
+    print('\t','hasilnya adalah =','{',a,',',b,',',c,'}')
+    print('\t','total = ',hasil)
+    
+    if hasil <= 10:
+         print ('\t','katagori : ( KECIL )','\n')
+         
+    elif hasil >= 11:
+         print ('\t','katagori : ( BESAR )','\n') 
+         
+def mundur(t):# waktu mundur 
+    while t:
+        mins, secs = divmod(t, 60) 
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print('\t','WAKTU : ','( ',timer,' )', end="\r")
+        sleep(1)
+        t -= 1
+
+for i in range (main):
+    print ('\t','---------------------')
+    print ('\t','| {••••BERMAIN••••} |')
+    print ('\t','---------------------','\n')
+    print ('\t',f"putaran dadu ke = {i}")
+    sc()
+    mundur(t)
+    
